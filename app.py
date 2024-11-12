2abdd4ff5bdad7eced8c36972f5a0973b67e80de
Daniel Luna <daniel.luna@coolblue.nl>
2024-11-12 13:23:43 +0100

    testing webhook

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def bot():
     # Create a comment with the random meme
     issue.create_comment(f"![Alt Text]({meme_url})")
     return "ok"
-
+#test
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
